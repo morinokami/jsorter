399b749e3c355a8e872022a00d5bd4c2ae3d1666
Shinya Fujino <shf0811@gmail.com>
2019-11-01 23:56:10 +0900

    Add format function

--- jsorter/jsorter.go
+++ jsorter/jsorter.go
@@ -9,6 +9,15 @@ var (
 	ErrInvalidJSON = errors.New("Invalid JSON")
 )
 
+var (
+	prefix = ""
+	indent = "  "
+)
+
+func format(jsonData interface{}) ([]byte, error) {
+	return json.MarshalIndent(jsonData, prefix, indent)
+}
+
 // Sort sorts the JSON data and returns the result.
 func Sort(jsonData []byte, reverse bool) ([]byte, error) {
 	if !json.Valid(jsonData) {
