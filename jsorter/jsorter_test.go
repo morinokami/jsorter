32586163a099effe85ba72f1f2a4bf4d88565289
Shinya Fujino <shf0811@gmail.com>
2019-11-02 00:20:02 +0900

    Add a test case for Sort

--- jsorter/jsorter_test.go
+++ jsorter/jsorter_test.go
@@ -1,36 +1,89 @@
 package jsorter
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestSort(t *testing.T) {
 	ts := []struct {
 		txt         string
-		orig        []byte
+		orig        string
 		reverse     bool
 		errExpected bool
 		exp         interface{}
 	}{
 		{
 			txt:         "invalid JSON 1",
-			orig:        []byte("I'm JSON"),
+			orig:        "I'm JSON",
 			errExpected: true,
 			exp:         ErrInvalidJSON,
 		},
 		{
 			txt:         "invalid JSON 2",
-			orig:        []byte(`{"example":2:]}}`),
+			orig:        `{"example":2:]}}`,
 			errExpected: true,
 			exp:         ErrInvalidJSON,
+		},
+		// https://json.org/example.html
+		{
+			txt: "valid JSON 1",
+			orig: `{
+  "glossary":{
+    "title":"example glossary",
+    "GlossDiv":{
+      "title":"S",
+      "GlossList":{
+        "GlossEntry":{
+          "ID":"SGML",
+          "SortAs":"SGML",
+          "GlossTerm":"Standard Generalized Markup Language",
+          "Acronym":"SGML",
+          "Abbrev":"ISO 8879:1986",
+          "GlossDef":{
+            "para":"A meta-markup language, used to create markup languages such as DocBook.",
+            "GlossSeeAlso":[
+              "GML",
+              "XML"
+            ]
+          },
+          "GlossSee":"markup"
+        }
+      }
+    }
+  }
+}`,
+			exp: `{
+  "glossary":{
+    "GlossDiv":{
+      "GlossList":{
+        "GlossEntry":{
+          "Abbrev":"ISO 8879:1986",
+          "Acronym":"SGML",
+          "GlossDef":{
+            "GlossSeeAlso":[
+              "GML",
+              "XML"
+            ],
+            "para":"A meta-markup language, used to create markup languages such as DocBook."
+          },
+          "GlossSee":"markup",
+          "GlossTerm":"Standard Generalized Markup Language",
+          "ID":"SGML",
+          "SortAs":"SGML"
+        }
+      },
+      "title":"S"
+    },
+    "title":"example glossary"
+  }
+}`,
 		},
 	}
 
 	for _, tc := range ts {
 		t.Log(tc.txt)
 
-		result, err := Sort(tc.orig, tc.reverse)
+		result, err := Sort([]byte(tc.orig), tc.reverse)
 
 		if tc.errExpected {
 			if err == nil {
@@ -46,10 +99,10 @@ func TestSort(t *testing.T) {
 			continue
 		}
 
-		if !reflect.DeepEqual(result, tc.exp) {
+		if string(result) != tc.exp {
 			t.Error("Not sorted as expected:")
-			t.Error(tc.exp)
-			t.Error(result)
+			t.Error("Expected:", tc.exp)
+			t.Error("Got:", result)
 		}
 	}
 }
